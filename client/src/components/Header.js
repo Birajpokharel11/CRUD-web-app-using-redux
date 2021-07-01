cf55e92d1b9290844ed83b81843d3b640f27d291
Biraj Pokharel <buraj.sainik@gmail.com>
2021-07-02 00:50:55 +0545

    first commit

---
+++ client/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import GoogleAuth from './GoogleAuth';
+
+const Header = () => {
+  return (
+    <div className="ui secondary pointing menu">
+      <Link to="/" className="item">
+        CRUD 
+      </Link>
+      <div className="right menu">
+        <Link to="/" className="item">
+         ALL forms
+        </Link>
+        <GoogleAuth />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
